102011a60e8af9a15254b10096bf2b0a3d111ade
Sergey Chikuyonok <serge.che@gmail.com>
2014-08-22 16:56:25 +0400

    Добавил модуль для сохранения состояния события

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ define(function(require, exports, module) {
 	var _ = require('lodash');
 	var postal = require('postal');
 	var facade = require('./lib/backbone-facade');
+	var preserve = require('./lib/preserve');
 
 	_.extend(postal.ChannelDefinition.prototype, facade);
-	return _.extend(postal, facade);
+	return _.extend(preserve(postal), facade);
 });
---
+++ test/preserve.js
@@ -0,0 +1,44 @@
+var assert = require('assert');
+var bus = require('../index');
+
+describe('Data preserve', function() {
+	it('default events', function() {
+		var a = 1;
+		bus.on('test', function(data) {
+			a += data;
+		});
+		bus.trigger('test', 2);
+		// подписались на событие после триггера: ничего не делаем
+		bus.on('test', function(data) {
+			a += data;
+		});
+
+		assert(a, 3);
+	});
+
+	it('persistent events', function() {
+		var a = 1, b = 1;
+		bus.on('test', function(data) {
+			a += data;
+		});
+		bus.trigger('!test', 2);
+		// подписались на событие после триггера форсированного
+		// события — вызовем его
+		bus.on('test', function(data) {
+			a += data;
+			b += data;
+		});
+		assert.equal(a, 5);
+		assert.equal(b, 3);
+
+		bus.trigger('test', 2);
+
+		// подписались после обычного события: ничего не делаем
+		bus.on('test', function(data) {
+			a += data;
+			b += data;
+		});
+		assert.equal(a, 9);
+		assert.equal(b, 5);
+	});
+});
